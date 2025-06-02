2585a0fc623bf0faf3f814b3f8af3af0af376b52
Jett <fawwazna26@gmail.com>
2025-06-02 21:18:29 +0700

    keys

---
+++ escapetheroom/src/keys.c
@@ -0,0 +1,16 @@
+#include "../include/keys.h"
+
+void initBagStack(bagStack *stack) {
+    stack->top = Nil;
+}
+
+void pushBag(bagStack *stack, infotype item) {
+    Bag *newBag = (Bag *)malloc(sizeof(Bag));
+    if (newBag == Nil) {
+        fprintf(stderr, "Memory allocation failed\n");
+        return;
+    }
+    newBag->items = item;
+    newBag->next = stack->top;
+    stack->top = newBag;
+}
